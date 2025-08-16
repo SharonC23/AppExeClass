97a5dc5ada042c5532030c678a6142d263820baa
SharonC23 <carrillosharon263@gmail.com>
2025-08-16 08:49:46 -0500

    Correciones

--- src/main/java/com/miApp/AppS/Impl/UserServiceImpl.java
+++ src/main/java/com/miApp/AppS/Impl/UserServiceImpl.java
@@ -1,23 +1,36 @@
 package com.miApp.AppS.Impl;
 
 import com.miApp.AppS.dto.UserDTO;
+import com.miApp.AppS.entity.User;
+import com.miApp.AppS.exceptions.CustomException;
 import com.miApp.AppS.repository.UserRepository;
 import com.miApp.AppS.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserServiceImpl() {
-        this.userRepository = null; // This is not a good practice, but included to match the constructor signature.
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(user -> modelMapper.map(user, UserDTO.class))
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -26,22 +39,40 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDTO createUser(UserDTO userDTO) {
-        return null;
+    public UserDTO getUserById(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException("User not found with id:" + userId));
+        return modelMapper.map(user, UserDTO.class);
     }
 
     @Override
-    public UserDTO getUserById(Long userId) {
-        return null;
+    public UserDTO createUser(UserDTO userDTO) {
+        if(userRepository.findById(userDTO.getUserId()).isPresent()) {
+            throw new CustomException("Email already exists");
+        }
+        User user = modelMapper.map(userDTO, User.class);
+        user = userRepository.save(user);
+        return modelMapper.map(user, UserDTO.class);
     }
 
     @Override
     public UserDTO updateUser(Long userId, UserDTO userDTO) {
-        return null;
+        User user = userRepository.findById(userDTO.getUserId())
+                .orElseThrow(() -> new CustomException("User not found with id:" + userId));
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        User updateUser = userRepository.save(user);
+        return modelMapper.map(updateUser, UserDTO.class);
+
     }
 
     @Override
-    public void deleteUser(Long userId) {
+    public boolean deleteUser(Long userId) {
+        if (! userRepository.existsById(userId)){
+            throw new CustomException("User not found with id:" + userId);
+        }
+        userRepository.deleteById(userId);
+        return true;
 
     }
 }
--- src/main/java/com/miApp/AppS/service/UserService.java
+++ src/main/java/com/miApp/AppS/service/UserService.java
@@ -2,7 +2,12 @@ package com.miApp.AppS.service;
 
 import com.miApp.AppS.dto.UserDTO;
 
+import java.util.List;
+
 public interface UserService {
+
+    List<UserDTO> getAllUsers();
+
     UserDTO findAllUsers();
 
     //UserDTO authenticateUser(String email, String password);
@@ -10,6 +15,6 @@ public interface UserService {
     UserDTO createUser(UserDTO userDTO);
     UserDTO getUserById(Long userId);
     UserDTO updateUser(Long userId, UserDTO userDTO);
-    void deleteUser(Long userId);
+    boolean deleteUser(Long userId);
 
 }
